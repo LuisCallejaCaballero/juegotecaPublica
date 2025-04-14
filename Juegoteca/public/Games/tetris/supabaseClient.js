765d01ed29a7a0e5ab93479e67c3937cf419354a
LuisCallejaCaballero <tepeleno2002@hotmail.es>
2025-04-14 18:55:47 +0200

    database2

---
+++ Juegoteca/public/Games/tetris/supabaseClient.js
@@ -0,0 +1,5 @@
+
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+
+export const supabase = createClient("https://xrmfmzifvpcdjukjgzdf.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhybWZtemlmdnBjZGp1a2pnemRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ2NDg0OTcsImV4cCI6MjA2MDIyNDQ5N30.X34V0puyQPBTBpLqAoiiWAP5yYaZI9Eop_rnYJwEd0s")
