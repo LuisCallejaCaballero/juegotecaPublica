577b47a5962a0267c8076c82ac0c482e4fec78e8
LuisCallejaCaballero <tepeleno2002@hotmail.es>
2025-04-14 16:49:40 +0200

    cambios

--- Juegoteca/src/components/Home.jsx
+++ Juegoteca/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import CardGame from "../components/CardGame";
-import { supabase } from "../assets/supabaseClient";
+//mport { supabase } from "../assets/supabaseClient";
 import HeroSlider from "../components/HeroSlider";
 import { Link } from "react-router-dom";
 import "../App.css";
